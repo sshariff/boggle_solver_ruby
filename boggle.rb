b2028e9ee81c8b296c6c787b9014636d7b080ef8
sss <sss@malachiarts.com>
2015-01-20 18:29:54 -0700

    added dictionary features, w/o stopping when beginning isn't part of a word

--- boggle.rb
+++ boggle.rb
@@ -1,5 +1,15 @@
-$size = 3
+require 'ruby-dictionary'
 
+$dictionary = Dictionary.new(%w( cab cabbage cad bad dab back backpack))
+
+puts $dictionary.exists?('cabb')
+
+puts $dictionary.starting_with('cabb')
+
+#$size = 3
+$size = 2
+$depth = 0
+$words = Array.new
 
 true3 = Array.new(3,true)
 
@@ -8,23 +18,21 @@ untried = Array.new($size*$size, true)
 
 
 #grid = [['a','f','t'],['b','a','g'],['c','a','t']]
-grid = ['a','f','t','b','a','g','c','a','t']
+#grid = ['a','b','c','d','e','f','g','h','i']
+grid = ['a','b','c','d']
 #subgrid = grid[1]
 #puts "subgrid is #{subgrid}"
 #subgrid[2]='z'
 #grid[1] = subgrid
 
 
-puts grid[0..2].join
-puts grid[3..5].join
-puts grid[6..8].join
+#puts grid[0..2].join
+#puts grid[3..5].join
+#puts grid[6..8].join
+puts grid[0..1].join
+puts grid[2..3].join
 
-stencil = {0=>[-1,-1], 1=>[0,-1], 2=>[1,1], 3=>[1,0], 4=>[1,1], 5=>[0,1],
-     6=>[-1,1], 7=>[-1,0]}
-
-tx = stencil[4][0] + stencil[7][0]
-ty = stencil[4][1] + stencil[7][1]
-puts tx, ty
+gets
 
 class Point
   attr_reader :x, :y
@@ -61,8 +69,18 @@ sum = zero+one
 $val = lambda { |a,i,j| a[$size*j + i]  }
 $elem = lambda { |i,j| $size*j + i }
 
-def traverse(index,untried)
+def traverse(index,untried,grid,string)
+
+  last=Point.new
+    puts "\n\nentering traverse"
+    puts "untried(0,1) is #{untried[0..1]}"
+    puts "untried(2,3) is #{untried[2..3]}"
+
+    puts "depth = #{$depth}, index is #{index}"
+ 
+ 
   (0..7).each {|s|
+       puts "s = #{s}"
     neighbor = index + $stencil[s]
 #      offset = Point.new(x,y)
 #      puts "i=#{i}, j=#{j}, s=#{s}, #{blah.class}"
@@ -71,19 +89,51 @@ def traverse(index,untried)
     if (x.between?(0,$size-1) && y.between?(0,$size-1) &&
         $val.call(untried,x,y) )
       puts "neighbor is #{neighbor}"
-    elsif !$val.call(untried,x,y)
+      letter = $val.call(grid,x,y)
+      puts "letter is #{letter}"
+      string += letter
+      if (string.length > 2 && $dictionary.exists?(string) )
+ #     if (string.length > 2 )
+ 
+     
+        $words.push(string)
+        puts "words is now #{$words}"
+      end
+      puts "string is #{string}"
+      last = index
+      index = neighbor
+      untried[$elem.call(x,y)] = false
+      puts "calling traverse, index is #{index}"
+      $depth += 1
+      traverse(index,untried,grid,string)
+      puts "string was #{string}"
+      string = string[0..-2]
+      puts "string is #{string}"
+      index = last
+    elsif ( !x.between?(0,$size-1) || !y.between?(0,$size-1) )
+      puts "out of bounds, #{x},#{y}"
+    else
       puts "unused #{x},#{y} is used"
+      puts "string is #{string}"
+      puts "returning"
     end
         
+      
   }
+  puts "\n\ndropping out one level of recursion, depth=#{$depth}"
+  untried[$elem.call(index.x,index.y)] = true
+  puts "string was #{string}, index is #{index}"
+  puts "untried(0,1) is #{untried[0..1]}"
+  puts "untried(2,3) is #{untried[2..3]}"
+  $depth -= 1  
 end
 
 puts "#{sum.x}, #{sum.y}"
 puts "\n Here we go\n\n"
 
-#(0...$size).each {|j|
-#  (0...$size).each {|i|
-    i=1; j=1
+(0...$size).each {|j|
+  (0...$size).each {|i|
+#    i=0; j=1
     
 #    unused = [[true,true,true],[true,true,true],[true,true,true]]
      untried = Array.new($size*$size,true)
@@ -105,6 +155,10 @@ puts "\n Here we go\n\n"
     puts "index is #{index}"
     
     puts "calling traverse"
-    traverse(index,untried)
- #   }
-#  }   
+    traverse(index,untried,grid,string)
+    }
+  }
+
+puts "+++++++++++++++++++++++++++++++"
+puts "The list of words is #{$words}"
+puts "+++++++++++++++++++++++++++++++"
